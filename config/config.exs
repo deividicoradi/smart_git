e490f3405c914c87a8bee88cfa85475764bd9414
deividicoradi <deividi.coradii@gmail.com>
2022-04-14 13:04:17 -0300

    config

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,11 @@
 # This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
+# and its dependencies with the aid of the Config module.
 #
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
 
 # General application configuration
-use Mix.Config
+import Config
 
 config :smart_git,
   ecto_repos: [SmartGit.Repo],
@@ -14,10 +14,32 @@ config :smart_git,
 # Configures the endpoint
 config :smart_git, SmartGitWeb.Endpoint,
   url: [host: "localhost"],
-  secret_key_base: "oVWLeI/KWnnNdJfMm80IMD/sT1FM7FMfDH+ssk3zwZlNUwfJ/9hlM/UlG5LMQG1V",
   render_errors: [view: SmartGitWeb.ErrorView, accepts: ~w(html json), layout: false],
   pubsub_server: SmartGit.PubSub,
-  live_view: [signing_salt: "cJtkID8m"]
+  live_view: [signing_salt: "JRpcCIBI"]
+
+# Configures the mailer
+#
+# By default it uses the "Local" adapter which stores the emails
+# locally. You can see the emails in your browser, at "/dev/mailbox".
+#
+# For production it's recommended to configure a different adapter
+# at the `config/runtime.exs`.
+config :smart_git, SmartGit.Mailer, adapter: Swoosh.Adapters.Local
+config :tesla, adapter: Tesla.Adapter.Hackney
+
+# Swoosh API client is needed for adapters other than SMTP.
+config :swoosh, :api_client, false
+
+# Configure esbuild (the version is required)
+config :esbuild,
+  version: "0.12.18",
+  default: [
+    args:
+      ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    cd: Path.expand("../assets", __DIR__),
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -29,4 +51,4 @@ config :phoenix, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+import_config "#{config_env()}.exs"
