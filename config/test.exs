9c921568a027721d51d6f07421b23e9d5bc8c99c
deividicoradi <deividi.coradii@gmail.com>
2022-04-14 14:09:11 -0300

    config

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,4 @@
-use Mix.Config
+import Config
 
 # Configure your database
 #
@@ -10,13 +10,23 @@ config :smart_git, SmartGit.Repo,
   password: "postgres",
   database: "smart_git_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :smart_git, SmartGitWeb.Endpoint,
-  http: [port: 4002],
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "Z2t33lb1mom6GVmfBcp5VcCpczbpO4UIXqdm6JHet818DNzNiwO0k/0bDAtwVxNy",
   server: false
+
+config :tesla, adapter: Tesla.Mock
+
+# In test we don't send emails.
+config :smart_git, SmartGit.Mailer, adapter: Swoosh.Adapters.Test
 
 # Print only warnings and errors during test
 config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
